93f7c043693e8638e3d4fd22bb32fc9a5fef0b5d
Oli Sturm <oliver@oliversturm.com>
2021-09-02 16:14:55 +0100

    swap tests

---
+++ source/Tests/SwapTests.cs
@@ -0,0 +1,49 @@
+// Copyright (C) 2008-2021 Oliver Sturm <oliver@oliversturm.com>
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, see <http://www.gnu.org/licenses/>.
+
+using NUnit.Framework;
+using FCSlib;
+
+namespace Tests;
+
+public class SwapTests {
+  [SetUp]
+  public void Setup() {
+  }
+
+  [Test]
+  public void SwapSameTypeArgs() {
+    var result = Functional.Swap<string, string, int>(one => two => {
+      Assert.AreEqual("one", two);
+      Assert.AreEqual("two", one);
+      return 42;
+    })("one")("two");
+
+    Assert.AreEqual(42, result);
+  }
+
+  [Test]
+  public void SwapDifferentTypeArgs() {
+    var result = Functional.Swap<string, int, bool>(one => two => {
+      Assert.AreEqual(42, two);
+      Assert.AreEqual("two", one);
+      return true;
+    })(42)("two");
+
+    Assert.IsTrue(result);
+  }
+
+}
+
