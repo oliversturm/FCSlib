8b7c9f47c6a3d924b3796158a4c9cddb76bc7acd
Oli Sturm <oliver@oliversturm.com>
2021-09-10 14:19:31 +0100

    syntax

--- source/FCSlib/Data/Range.cs
+++ source/FCSlib/Data/Range.cs
@@ -17,108 +17,91 @@ using System.Collections;
 
 namespace FCSlib.Data {
   public static class Range {
-    private static byte GetNext(byte val) { return (byte)(val + 1); }
-    private static short GetNext(short val) { return (short)(val + 1); }
-    private static int GetNext(int val) { return val + 1; }
-    private static long GetNext(long val) { return val + 1; }
-    private static sbyte GetNext(sbyte val) { return (sbyte)(val + 1); }
-    private static ushort GetNext(ushort val) { return (ushort)(val + 1); }
-    private static uint GetNext(uint val) { return val + 1; }
-    private static ulong GetNext(ulong val) { return val + 1; }
-    private static double GetNext(double val) { return val + 1; }
-    private static float GetNext(float val) { return val + 1; }
-    private static decimal GetNext(decimal val) { return val + 1; }
-    private static DateTime GetNext(DateTime val) { return val.AddDays(1); }
-    private static char GetNext(char val) { return (char)(val + 1); }
+    private static byte GetNext(byte val) => (byte)(val + 1);
+    private static short GetNext(short val) => (short)(val + 1);
+    private static int GetNext(int val) => val + 1;
+    private static long GetNext(long val) => val + 1;
+    private static sbyte GetNext(sbyte val) => (sbyte)(val + 1);
+    private static ushort GetNext(ushort val) => (ushort)(val + 1);
+    private static uint GetNext(uint val) => val + 1;
+    private static ulong GetNext(ulong val) => val + 1;
+    private static double GetNext(double val) => val + 1;
+    private static float GetNext(float val) => val + 1;
+    private static decimal GetNext(decimal val) => val + 1;
+    private static DateTime GetNext(DateTime val) => val.AddDays(1);
+    private static char GetNext(char val) => (char)(val + 1);
 
+    public static Range<byte> Create(byte start, byte end) =>
+      new(start, end, GetNext);
 
+    public static Range<short> Create(short start, short end) =>
+      new(start, end, GetNext);
 
-    public static Range<byte> Create(byte start, byte end) {
-      return new Range<byte>(start, end, GetNext);
-    }
-    public static Range<short> Create(short start, short end) {
-      return new Range<short>(start, end, GetNext);
-    }
-    public static Range<int> Create(int start, int end) {
-      return new Range<int>(start, end, GetNext);
-    }
-    public static Range<long> Create(long start, long end) {
-      return new Range<long>(start, end, GetNext);
-    }
-    public static Range<sbyte> Create(sbyte start, sbyte end) {
-      return new Range<sbyte>(start, end, GetNext);
-    }
-    public static Range<ushort> Create(ushort start, ushort end) {
-      return new Range<ushort>(start, end, GetNext);
-    }
-    public static Range<uint> Create(uint start, uint end) {
-      return new Range<uint>(start, end, GetNext);
-    }
-    public static Range<ulong> Create(ulong start, ulong end) {
-      return new Range<ulong>(start, end, GetNext);
-    }
-    public static Range<double> Create(double start, double end) {
-      return new Range<double>(start, end, GetNext);
-    }
-    public static Range<float> Create(float start, float end) {
-      return new Range<float>(start, end, GetNext);
-    }
-    public static Range<decimal> Create(decimal start, decimal end) {
-      return new Range<decimal>(start, end, GetNext);
-    }
-    public static Range<char> Create(char start, char end) {
-      return new Range<char>(start, end, GetNext);
-    }
-    public static Range<DateTime> Create(DateTime start, DateTime end) {
-      return new Range<DateTime>(start, end, GetNext);
-    }
+    public static Range<int> Create(int start, int end) =>
+      new(start, end, GetNext);
 
-    public static Range<T> Create<T>(T start, T end, Func<T, T> getNext, Comparison<T> compare) {
-      return new Range<T>(start, end, getNext, compare);
-    }
-    public static Range<T> Create<T>(T start, T end, Func<T, T> getNext) {
-      return new Range<T>(start, end, getNext);
-    }
+    public static Range<long> Create(long start, long end) =>
+      new(start, end, GetNext);
+
+    public static Range<sbyte> Create(sbyte start, sbyte end) =>
+      new(start, end, GetNext);
+
+    public static Range<ushort> Create(ushort start, ushort end) =>
+      new(start, end, GetNext);
+
+    public static Range<uint> Create(uint start, uint end) =>
+      new(start, end, GetNext);
+
+    public static Range<ulong> Create(ulong start, ulong end) =>
+      new(start, end, GetNext);
+
+    public static Range<double> Create(double start, double end) =>
+      new(start, end, GetNext);
+
+    public static Range<float> Create(float start, float end) =>
+      new(start, end, GetNext);
+
+    public static Range<decimal> Create(decimal start, decimal end) =>
+      new(start, end, GetNext);
+
+    public static Range<char> Create(char start, char end) =>
+      new(start, end, GetNext);
+
+    public static Range<DateTime> Create(DateTime start, DateTime end) =>
+      new(start, end, GetNext);
+
+    public static Range<T> Create<T>(T start, T end, Func<T, T> getNext, Comparison<T> compare) =>
+      new(start, end, getNext, compare);
+
+    public static Range<T> Create<T>(T start, T end, Func<T, T> getNext) =>
+      new(start, end, getNext);
 
   }
 
   public class Range<T> : IRange<T> {
     public Range(T start, T end, Func<T, T> getNext, Comparison<T> compare) {
-      this.start = start;
-      this.end = end;
+      this.Start = start;
+      this.End = end;
       this.compare = compare;
       this.sequence = Functional.Sequence<T>(getNext, start, v => compare(getNext(v), end) > 0);
     }
 
     public Range(T start, T end, Func<T, T> getNext) : this(start, end, getNext, Compare) { }
 
-    private static int Compare<U>(U one, U other) {
-      return Comparer<U>.Default.Compare(one, other);
-    }
+    private static int Compare<U>(U one, U other) =>
+      Comparer<U>.Default.Compare(one, other);
 
     readonly Comparison<T> compare;
     readonly IEnumerable<T> sequence;
 
-    readonly T start;
-    public T Start {
-      get { return start; }
-    }
+    public T Start { get; init; }
+    public T End { get; init; }
 
-    readonly T end;
-    public T End {
-      get { return end; }
-    }
+    public bool Contains(T value) =>
+      compare(value, Start) >= 0 && compare(End, value) >= 0;
 
-    public bool Contains(T value) {
-      return compare(value, start) >= 0 && compare(end, value) >= 0;
-    }
+    IEnumerator<T> IEnumerable<T>.GetEnumerator() => sequence.GetEnumerator();
 
-    IEnumerator<T> IEnumerable<T>.GetEnumerator() {
-      return sequence.GetEnumerator();
-    }
-
-    IEnumerator IEnumerable.GetEnumerator() {
-      return ((IEnumerable<T>)this).GetEnumerator();
-    }
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable<T>)this).GetEnumerator();
   }
 }
